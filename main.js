d144bad0bd2300583870abc1263c3697bc86c713
anlegent <alegent@edenschool.fr>
2024-11-06 11:06:16 +0100

    fixes and added a discount calculator

--- main.js
+++ main.js
@@ -1,107 +1,129 @@
 // 1
-function addition(a, b) {
-  return a + b;
-}
-console.log(addition(2, 3)); // Résultat attendu : 5
+//function addition(a, b) {
+//  return a + b;
+//}
+//console.log(addition(2, 3)); // Résultat attendu : 5
 
 // 2
-function salutation(nom) {
-  return "bonjour" + " " + nom;
-}
-console.log(salutation("Alice")); // Résultat attendu : "Bonjour Alice"
+//function salutation(nom) {
+//  return "bonjour" + " " + nom;
+//}
+//console.log(salutation("Alice")); // Résultat attendu : "Bonjour Alice"
 
 // 3
-function estPair(nombre) {
-  return nombre / 1;
-}
-console.log(estPair(4)); // Résultat attendu : true
-console.log(estPair(7)); // Résultat attendu : false
+//function estPair(nombre) {
+//  return nombre / 1;
+//}
+//console.log(estPair(4)); // Résultat attendu : true
+//console.log(estPair(7)); // Résultat attendu : false
 
 // 4
-function aireRectangle(longueur, largeur) {
-  return longueur * largeur;
-}
-console.log(aireRectangle(5, 3)); // Résultat attendu : 15
+//function aireRectangle(longueur, largeur) {
+//  return longueur * largeur;
+//}
+//console.log(aireRectangle(5, 3)); // Résultat attendu : 15
 
 // 5
-function estMajuscule(chaine) {
-  return chaine == chaine.toUpperCase();
-}
-console.log(estMajuscule("HELLO")); // Résultat attendu : true
-console.log(estMajuscule("Hello")); // Résultat attendu : false
+//function estMajuscule(chaine) {
+//  return chaine == chaine.toUpperCase();
+//}
+//console.log(estMajuscule("HELLO")); // Résultat attendu : true
+//console.log(estMajuscule("Hello")); // Résultat attendu : false
 
 // 6
-function maxDeuxNombres(a, b) {
-  return Math.max(a, b);
-}
-console.log(maxDeuxNombres(5, 10)); // Résultat attendu : 10
+//function maxDeuxNombres(a, b) {
+//  return Math.max(a, b);
+//}
+//console.log(maxDeuxNombres(5, 10)); // Résultat attendu : 10
 
 // 7
-function diffDeuxNombres(a, b) {
-  return b - a;
-}
-console.log(diffDeuxNombres(5, 10)); // Résultat attendu : 10
+//function diffDeuxNombres(a, b) {
+//  return b - a;
+//}
+//console.log(diffDeuxNombres(5, 10)); // Résultat attendu : 10
 
 // 8
-function convertirEnCelsius(fahrenheit) {
-  return ((fahrenheit - 32) * 5) / 9;
-}
-console.log(convertirEnCelsius(100)); // Résultat attendu : 37.7778
+//function convertirEnCelsius(fahrenheit) {
+//  return ((fahrenheit - 32) * 5) / 9;
+//}
+//console.log(convertirEnCelsius(100)); // Résultat attendu : 37.7778
 
 // 1.2
-function sumTwoNumber(a, b) {
-  return a + b;
-}
-console.log(sumTwoNumber(10, 10));
+//function sumTwoNumber(a, b) {
+//  return a + b;
+//}
+//console.log(sumTwoNumber(10, 10));
 
 // 2.2
-function concatenateStrings(str1, str2) {
-  return str1 + str2;
-}
-console.log(concatenateStrings("Bonjour", "Bonsoir"));
+//function concatenateStrings(str1, str2) {
+//  return str1 + str2;
+//}
+//console.log(concatenateStrings("Bonjour", "Bonsoir"));
 
 // 3.2
-function isEven(n) {
-  return n % 2 == 0;
-}
-console.log(isEven(5));
+//function isEven(n) {
+//  return n % 2 == 0;
+//}
+//console.log(isEven(5));
 
 // 4.2
-function rectangleArea(width, height) {
-  return width * height;
-}
-console.log(rectangleArea(10, 15));
+//function rectangleArea(width, height) {
+//  return width * height;
+//}
+//console.log(rectangleArea(10, 15));
 
 // 5.2
-function isAdult(age) {
-  return age >= 18;
-}
-console.log(isAdult(15));
+//function isAdult(age) {
+//  return age >= 18;
+//}
+//console.log(isAdult(15));
 
 // 6.2
-function isPositive(n) {
-  return n > 0;
-}
-console.log(isPositive(-10));
+//function isPositive(n) {
+//  return n > 0;
+//}
+//console.log(isPositive(-10));
 
 // 7.2
-function minTwoNumbers(a, b) {
-  return Math.min(a, b);
-}
-console.log(minTwoNumbers(20, 10));
+//function minTwoNumbers(a, b) {
+//  return Math.min(a, b);
+//}
+//console.log(minTwoNumbers(20, 10));
 
 // 8.2
 //function isPalindrome(str) {}
 //console.log(isPalindrome("kayak"));
 
 // 9.2
-function maxThreeNumbers(a, b, c) {
-  return Math.max(a, b, c);
-}
-console.log(maxThreeNumbers(67, 1500, 999));
+//function maxThreeNumbers(a, b, c) {
+//  return Math.max(a, b, c);
+//}
+//console.log(maxThreeNumbers(67, 1500, 999));
 
 // 10.2
-function multiplyTwoNumbers(a, b) {
-  return a * b;
+//function multiplyTwoNumbers(a, b) {
+//  return a * b;
+//}
+//console.log(multiplyTwoNumbers(100, 1));
+
+// discount calculator
+let price = prompt("price");
+if (price <= 0 || NaN) {
+  alert("Price not valid");
 }
-console.log(multiplyTwoNumbers(100, 1));
+
+let remiseEtudiant = price - (15 * price) / 100;
+let remiseSoldes = price - (20 * price) / 100;
+let remiseFidelite = price - (10 * price) / 100;
+let aucuneRemise = price - (0 * price) / 100;
+
+let discount = prompt("discount");
+if (discount == "etudiant") {
+  alert(remiseEtudiant + " $");
+} else if (discount == "soldes") {
+  alert(remiseSoldes + " $");
+} else if (discount == "fidelite") {
+  alert(remiseFidelite + " $");
+} else if (discount == "aucune") {
+  alert(aucuneRemise + " $");
+}
